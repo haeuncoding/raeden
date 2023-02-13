27dc3a50303c140f72d132e71d55223160024066
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2023-02-12 23:17:43 -0800

    adding frontend

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,23 +1,44 @@
 class ListsController < ActionController::API
 
   def index
-
+    @lists = List.all
+    if @lists
+      render :index
+    else
+      render {}
+    end
   end
 
   def create
-
+    @list = List.new(list_params)
+    if @list.save
+      render :show
+    else
+      render json: [errors: @list.errors.full_messages]
+    end
   end
 
   def show
-
+    @list = list.find_by(id: params[:id])
+    render :show
   end
 
   def update
-
+    @list = list.find_by(id: params[:id])
+    if @list.update(list_params)
+      render :show
+    else
+      render json: @list.errors.full_messages
+    end
   end
 
   def destroy
+    @list = list.find_by(id: params[:id])
+    @list.delete
+  end
 
+  def add_book
+    book = Book.find_by(id: params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :books, only: [:index, :create, :show, :update, :destroy]
     resources :lists, only: [:index, :create, :show, :update, :destroy]
+    
   end
 
   get '*path', to: 'static_pages#frontend'
